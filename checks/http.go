b5c1ce7e8ec703460a1eca1ece714c24ed03cb4a
Edo Aria <edo@dinkuminteractive.com>
2015-09-07 17:58:05 +0700

    get all data

--- checks/http.go
+++ checks/http.go
@@ -1,14 +1,16 @@
 package checks
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
-	"io"
-	"net"
-	"net/http"
+	"log"
 	"strconv"
 	"time"
+	"net"
+	"net/http"
+	"io/ioutil"
+	"bufio"
+	"strings"
+	//compress/gzip
 )
 
 // HTTPResult struct
@@ -19,7 +21,7 @@ type HTTPResult struct {
 	Wait       *float64 `json:"wait"`
 	Recv       *float64 `json:"recv"`
 	Total      *float64 `json:"total"`
-	DataLength int      `json:"dataLength"`
+	DataLength *float64 `json:"dataLength"`
 	StatusCode *float64 `json:"statusCode"`
 	Error      *string  `json:"error"`
 }
@@ -40,10 +42,12 @@ func NewRaintankHTTPProbe(body []byte) (*RaintankProbeHTTP, error) {
 	p := RaintankProbeHTTP{}
 	err := json.Unmarshal(body, &p)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse settings. " + err.Error())
+		log.Fatalf("failed to parse settings. %v", err.Error())
+		return nil, err
 	}
 	if port, err := strconv.ParseInt(p.Port, 10, 32); err != nil || port < 1 || port > 65535 {
-		return nil, fmt.Errorf("failed to parse settings. Invalid port")
+		log.Fatal("failed to parse settings. Invalid port")
+		return nil, err
 	}
 	return &p, nil
 }
@@ -56,27 +60,31 @@ func (p *RaintankProbeHTTP) Results() interface{} {
 // Run checking
 func (p *RaintankProbeHTTP) Run() error {
 	p.Result = &HTTPResult{}
-
-	request, err := http.NewRequest(p.Method, p.Host+p.Path, nil)
+	
+	// reader
+	reader := bufio.NewReader(strings.NewReader(p.Method+" / HTTP/1.0\r\nHost: "+p.Host+"\r\n\r\n"))
+	request, err := http.NewRequest(p.Method, "http://"+p.Host+p.Path, reader)
+	request.Header.Set("Connection", "close")
+	request.Header.Set("Accept-Encpoding", "gzip")
+	request.Header.Set("User-Agent", "raintank-probe")
+	
 	if err != nil {
-		msg := "connection closed" //err.Error()
+		msg := "connection closed"
 		p.Result.Error = &msg
 		return nil
 	}
-
-	start := time.Now()
+	
 	// Dialing
-	request.Header.Set("Connection", "close")
+	start := time.Now()
 	conn, err := net.Dial("tcp", p.Host+":"+p.Port)
 	if err != nil {
 		msg := err.Error()
 		p.Result.Error = &msg
 		return nil
 	}
-	defer conn.Close()
-	duration := time.Since(start).Seconds()
+	duration := time.Since(start).Seconds() * 1000
 	p.Result.Connect = &duration
-
+	
 	// DNS
 	step := time.Now()
 	addrs, err := net.LookupHost(p.Host)
@@ -85,7 +93,7 @@ func (p *RaintankProbeHTTP) Run() error {
 		p.Result.Error = &msg
 		return nil
 	}
-	duration = time.Since(step).Seconds()
+	duration = time.Since(step).Seconds() * 1000
 	p.Result.DNS = &duration
 
 	// Send
@@ -95,36 +103,78 @@ func (p *RaintankProbeHTTP) Run() error {
 		p.Result.Error = &msg
 		return nil
 	}
-	send := time.Since(step).Seconds()
+	send := time.Since(step).Seconds() * 1000
 	p.Result.Send = &send
-
+	
 	// Wait
 	step = time.Now()
 	firstByte := make([]byte, 1)
-	bytesRead, err := conn.Read(firstByte)
+	_, err = conn.Read(firstByte)
 	if err != nil {
 		msg := err.Error()
 		p.Result.Error = &msg
 		return nil
 	}
-	p.Result.DataLength = bytesRead
-	wait := time.Since(step).Seconds()
+	wait := time.Since(step).Seconds() * 1000
 	p.Result.Wait = &wait
-
+	defer conn.Close()
+	
 	// Receive
 	step = time.Now()
-	var buf bytes.Buffer
-	buf.Write(firstByte)
-	_, err = io.Copy(&buf, conn)
+	client := &http.Client{}
+	response, _ := client.Do(request)
+	statusCode := float64(response.StatusCode)
+    p.Result.StatusCode = &statusCode 
+	body, err := ioutil.ReadAll(response.Body)
+	dataLength := float64(len(body))
+	p.Result.DataLength = &dataLength
+	
+	log.Println(response)
+    // uri, err := url.ParseRequestURI("http://" + p.Host + ":" + p.Port)
+	// uri.Path = p.Path
+	
+	// data := url.Values{}
+    // uriStr := fmt.Sprintf("%v", uri)
+	
+	// 
+    // req, _ := http.NewRequest(p.Method, uriStr, bytes.NewBufferString(data.Encode()))
+	// req.Header.Set("Connection", "close")
+	// client := &http.Client{}
+    // response, _ := client.Do(request)
+	
+	// // length := response.Header.Get("Content-Length")
+	// // fmt.Println(length)
+	
+	// body, err := ioutil.ReadAll(response.Body)
+    // fmt.Println(string(body))
+	// header := []byte(p.Method + " " + p.Path + " HTTP/1.1\r\nHost: " + p.Host + "\r\n\r\n")
+    // conn.Write(header)
+	
+	// scanner := bufio.NewScanner(conn)
+    // firstLine := true
+    // bytesRead := ""
+    
+    // for scanner.Scan() {
+    //     if firstLine == true {
+    //         firstLine = false
+    //         //p.Result.StatusCode = scanner.Text()
+    //     }
+    //     bytesRead += scanner.Text()
+    // }
 	if err != nil {
 		msg := err.Error()
 		p.Result.Error = &msg
 		return nil
 	}
-	recv := time.Since(step).Seconds()
+	recv := time.Since(step).Seconds() * 1000
+	
+	// dataLength := float64(response.ContentLength)
+	// p.Result.DataLength = &dataLength
 	p.Result.Recv = &recv
-
-	total := time.Since(start).Seconds()
+	
+	// total time
+	total := time.Since(start).Seconds() * 1000
 	p.Result.Total = &total
+	
 	return nil
 }
