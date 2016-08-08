bd6e6a5e2d4aecc96c06e784e9d25968d781248e
woodsaj <anthony@monkey.id.au>
2016-08-08 17:09:57 +0800

    build docker image

---
+++ scripts/build_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+# Find the directory we exist within
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+VERSION=`git describe --always`
+
+mkdir build
+cp ../build/raintank-probe build/
+
+docker build -t raintank/raintank-probe:$VERSION .
+docker tag raintank/raintank-probe:$VERSION raintank/raintank-probe:latest
